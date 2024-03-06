9b035efc5fc2534806999af4522229deac412435
NaginaAfzali8is <134254650+NaginaAfzali8is@users.noreply.github.com>
2024-03-06 11:12:29 +0500

    update next

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 yum install -y nodejs npm
-cd /home/ubuntu/CICD_TEST2
 npm install
--- scripts/start_nodejs_server.sh
+++ scripts/start_nodejs_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ubuntu/CICD_TEST2/app.js
+cd /home/ubuntu/CICD_TEST2/
 node app.js &
