11fb80bcefe14e3d1ad91cefa4cea4888cc58d82
NaginaAfzali8is <134254650+NaginaAfzali8is@users.noreply.github.com>
2024-03-06 10:47:35 +0500

    before install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-#!/bin/bash
-yum install -y httpd
-# Install node.js
-apt-get update
-apt-get install nodejs -y
 
-# Install forever module 
-# https://www.npmjs.com/package/forever
-npm install forever -g
+# Install Apache HTTP Server
+# Adjust the installation command based on your package manager
+# For CentOS/RHEL (yum):
+# yum install -y httpd
+# For Ubuntu/Debian (apt-get):
+apt-get update
+apt-get install -y apache2
+
+# Install Node.js
+# Adjust the installation command based on your package manager
+# For CentOS/RHEL (yum):
+# <installation command for Node.js>
+# For Ubuntu/Debian (apt-get):
+apt-get install -y nodejs
+
+# Install the forever module globally
+npm install -g forever
 
 # Clean working folder
-# find /home/ubuntu/test -type f -delete
+# This line may not be necessary, depending on your specific requirements.
+# Make sure to use the correct directory path.
+# find /path/to/your/working/folder -type f -delete
