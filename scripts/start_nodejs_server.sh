ef429d3737fce1b9483fb849f90088f62dd68553
NaginaAfzali8is <134254650+NaginaAfzali8is@users.noreply.github.com>
2024-03-06 10:59:21 +0500

    try next

--- scripts/start_nodejs_server.sh
+++ scripts/start_nodejs_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-yum install -y nodejs npm
 cd /path/to/your/nodejs/project
-npm install
+node server.js &
---
+++ scripts/stop_nodejs_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+isExistApp=$(pgrep node)
+if [[ -n $isExistApp ]]; then
+    kill $isExistApp
+fi
